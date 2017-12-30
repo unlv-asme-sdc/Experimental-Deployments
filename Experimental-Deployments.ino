b81506aaf70370d2d0a9a0d3d648e6e50d274b91
Allen Izquierdo <izquia1@unlv.nevada.edu>
2017-12-30 13:16:11 -0800

    Add MiniMaestroService Support

--- Experimental-Deployments.ino
+++ Experimental-Deployments.ino
@@ -19,11 +19,11 @@ PololuG2 rightmotor1 = PololuG2(2, 3, 4);
 PololuG2 rightmotor2 = PololuG2(5, 6, 7);
 PololuG2 leftmotor1 = PololuG2(8, 9, 10);
 PololuG2 leftmotor2 = PololuG2(13, 11, 12);
-//SoftwareSerial maestroSerial(18, 19); // Connect A1 to Maestro's RX. A0 must remain disconnected.
-//iniMaestroService maestro(maestroSerial);
-//HS485 chamber = HS485(maestro, 0);
-//HS485 intake = HS485(maestro, 1);
-//TalonSR shooter = TalonSR(maestro, 2);
+SoftwareSerial maestroSerial(18, 19); // Connect A1 to Maestro's RX. A0 must remain disconnected.
+MiniMaestroService maestro(maestroSerial);
+HS485 chamber = HS485(maestro, 0);
+HS485 intake = HS485(maestro, 1);
+TalonSR shooter = TalonSR(maestro, 2);
 TankDrive chassis = TankDrive(&leftmotor1, &leftmotor2, &rightmotor1, &rightmotor2);
 PS2X ps2x;
 NetworkTable network = NetworkTable(10, 10);
@@ -34,7 +34,7 @@ float rightvalue = 0;
 unsigned long last_blink;
 unsigned long last_update;
 void setup() {
-  //maestroSerial.begin(115200);
+  maestroSerial.begin(115200);
   ps2x.config_gamepad();
   ps2x.read_gamepad();
   Serial.begin(115200);
@@ -53,7 +53,6 @@ void ledService()
   if(value > 500)
   {
     last_blink = millis();
-    Serial.println("working");
   }
 }
 
@@ -62,9 +61,6 @@ void loop() {
   myPacketSerial.update();
   ledService();
   chassis.drive(ps2x);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
   
   if(network.getLastPS2PacketTime() > 500)
   {
@@ -78,7 +74,7 @@ void loop() {
     digitalWrite(8, HIGH);
     digitalWrite(13, HIGH);
   }
-  /*
+  
   if(millis() - last_update > 20)
   {
     int cross = ps2x.Button(PSB_CROSS);
@@ -86,7 +82,6 @@ void loop() {
     int triangle = ps2x.Button(PSB_TRIANGLE);
     int square = ps2x.Button(PSB_SQUARE);
     
-    /*
     if(cross)
     {
       shooter.setPower(-1);
@@ -105,7 +100,7 @@ void loop() {
     }
     last_update = millis();
   }
-  */
-  //maestro.service();
+
+  maestro.service();
 }
 
